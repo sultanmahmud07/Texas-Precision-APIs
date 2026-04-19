f911f3d8c398e2c1609128cf5cf6059462cb57f1
sultanmahmud07 <asadkhan01862@gmail.com>
2026-04-19 18:36:55 +0600

    Make location apis

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,6 +6,7 @@ import { ContactRoutes } from "../modules/contact/contact.route"
 import { OtpRoutes } from "../modules/otp/otp.route"
 import { ReviewRoutes } from "../modules/review/review.route"
 import { AvailabilityRoutes } from "../modules/availability/availability.route"
+import { AddressRoutes } from "../modules/address/address.route"
 
 export const router = Router()
 
@@ -18,13 +19,17 @@ const moduleRoutes = [
         path: "/auth",
         route: AuthRoutes
     },
-     {
+    {
         path: "/availability",
         route: AvailabilityRoutes
-    },  
+    },
     {
         path: "/review",
         route: ReviewRoutes
+    },
+    {
+        path: "/address",
+        route: AddressRoutes
     },
     {
         path: "/otp",
